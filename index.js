a7b52c17333710b140e57c20f5905cf1b4901ae5
mikebendorf11 <mikebendorf11@gmail.com>
2019-09-10 23:33:52 -0700

    double char, figurative props

--- index.js
+++ index.js
@@ -3,10 +3,78 @@ const readline = require('readline');
 
 
 var file = fs.readFileSync("mychars.json");
-var content = JSON.parse(file);
+var units = JSON.parse(file);
 
+1 || 1
+var newUnits = []
+
+for(let i = 50, j=0; i< 60; i++, j++){
+  var unit = {char:{}, short:{}, long:{}}
+  newUnits.push(unit)
+  var oldSingle = units[i].definitions.single[0]
+  var isDoubleChar = units[i].char.length > 1 || false
+  
+  //parenthesis 
+  oldSingle = isDoubleChar ? oldSingle.substring(1, oldSingle.length-2) : oldSingle
+  //single char doesn't have figurative
+  if(!isDoubleChar)
+    newUnits[j].char.figurative = undefined
+  else  
+    newUnits[j].char.figurative = units[i].definitions.single[1].trim()
+
+  //empty combs, leave complete
+  var oldCombShort = units[i].combinations.short
+  var oldDefShort = units[i].definitions.short
+  var oldCombLong = units[i].combinations.long
+  var oldDefLong = units[i].definitions.long
+
+  if(oldCombShort.length > 1){
+    for(let i=oldCombShort.length-1; i>-1; i--){
+      if(!oldCombShort[i]){
+        oldCombShort = oldCombShort.slice(0,i)
+        oldDefShort = oldDefShort.slice(0,i)
+      }
+    }
+  }
+  if(oldCombLong.length > 1){
+    for(let i=oldCombLong.length-1; i>-1; i--){
+      if(!oldCombLong[i]){
+        oldCombLong = oldCombLong.slice(0,i)
+        oldDefLong = oldDefLong.slice(0,i)
+      }
+    }
+  }
+  
+  newUnits[j].id = units[i].id
+  newUnits[j].char.hanzi = units[i].char
+  newUnits[j].char.literal = oldSingle
+  
+  newUnits[j].short.hanzi = units[i].combinations.short
+  console.dir(newUnits[j])
+}
+
+console.dir(newUnits)
+1 || 1
+
+
+
+
+/**
+ * definitions.single[0] = char.literal
+ * definitions.single[1] = char.figurative
+ * combinations.short = short.hanzi
+ * combinations.long = long.hanzi
+ * definitions.short = short.figurative
+ * definitions.long = long.figurative
+ * id, level, learnedId, level, consult => same
+ * learnedId determines is char is pending 
+ * short and long.pinyin dinamically calc from char.literal
+ *    ['a,b','c,d','e,f'] each is a str of comma separated char.pinyin
+ * char.trace 
+ */
+
+ /* //UNICODE DB PARSE
 var myCharsInUnicode = []
-
 content.forEach(element => {
   myCharsInUnicode.push(element.char.codePointAt(0).toString(16))
   //console.log(element.char.codePointAt(0).toString(16))
@@ -24,4 +92,4 @@ rl.on('line', function(line) {
   var arr = line.split('	')
   if(arr[1]=='kMandarin' || arr[1]=='kDefinition')
   console.log();
-});
+});*/
