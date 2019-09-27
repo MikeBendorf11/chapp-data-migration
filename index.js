55101463782d703f1a570da09278e77de288169c
mikebendorf11 <mikebendorf11@gmail.com>
2019-09-27 12:45:19 -0700

    Trimming non-hanzi chars out of short, todo:get all chars from unicode, regex to messy

--- index.js
+++ index.js
@@ -6,14 +6,32 @@ const readline = require('readline');
 // var units = JSON.stringify(JSON.parse(fileChNw))
 // 1==2
 // fs.writeFileSync('./units-new-struct.json.min',units)
-// 1==2
+
+//unicode just chars all (easier to find that regex for hanzi code)
+
+let rl = readline.createInterface({
+  input: fs.createReadStream('./extra/unicode.txt')
+ });
+
+ rl.on('line', function(line) {
+  var arr = line.split('	')
+  var code = arr[0].split('+')[1]
+  var charFromCode = code ? String.fromCodePoint(`0x${code}`) : 0x9C8B
+  console.log(charFromCode)
+ })
+
+ rl.on('close', ()=>{
+  1==2
+ })
+
 
 //singles
 var fileChNw = fs.readFileSync('./units-new-struct.json.min')
 var units = JSON.parse(
   fileChNw.toString()
   .split('&nbsp;').join('') //remove nbsp
-  
+  .split(/\(([^)]+)\)/g).join('')
+  .split('#').join('')
 )
 
 var single = {}
@@ -25,15 +43,30 @@ units.forEach((unit)=>{
   if(unit.learnedId && unit.char.hanzi.length==1){
     single[unit.char.hanzi] = {children: unit.short.hanzi.concat(unit.long.hanzi)}
     if(unit.short.hanzi.length>0){
-      unit.short.hanzi.forEach(sht=>{
-        if(!short[sht]){
-          short[sht] = {children:[]}
+      unit.short.hanzi.forEach(sht=>{ //create short[x]
+        if(sht.includes('/')){ //split to more sht arrs
+          sht.split('/').forEach(s=>{
+            if(!short[s]){
+              short[s] = {children:[]}
+            }
+          })
         }
+        else if(sht.includes(',')){ //split to more sht arrs
+          sht.split(',').forEach(s=>{
+            if(!short[s]){
+              short[s] = {children:[]}
+            }
+          })
+        }
+        else if(!short[sht]){ //add the sht chars length=1
+          short[sht] ={children:[]}
+        }
+
         if(unit.long.hanzi.length>0){
           unit.long.hanzi.forEach(lng=>{
             if(lng.includes(sht)){
               short[sht].children.push(lng)
-            }  
+            }
           })
         }
       })
@@ -42,3 +75,5 @@ units.forEach((unit)=>{
 })
 
 1==2
+fs.writeFileSync('./tree.json', JSON.stringify({single, short, long}, null, 2))
+
